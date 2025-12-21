0055a4b50b12cc9fd2eff8cf62e897713823788d
El hadji Rawane Meissa Sow <rawane.meissa@gmail.com>
2025-12-21 00:22:14 +0000

    feat: add favicon for improved branding and user experience

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,7 @@ export default function Document() {
         {/* Meta tags for optimization */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <link rel="icon" href="/assets/novitech.ico" type="image/x-icon" />
       </Head>
       <body>
         <Main />
